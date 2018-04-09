0e4c74209749a214125a71f6ce9fe7f1aed1e9c6
ShawnNew <ncxinhanzhong@gmail.com>
2018-04-09 00:44:11 -0400

    assignment4 bootstrap done

--- assignment4/code/Q1.R
+++ assignment4/code/Q1.R
@@ -6,8 +6,18 @@ len <- length(x)
 # Exact Confident Interval
 CI_exact <- c(qgamma(alpha/2, len) / (len * mean(x)), qgamma(1 - alpha/2, len) / (len * mean(x)))
 # CI from LRT
-#CI_LRT <- c()
+f <- function(lambda) lambda*mean(x)-log(lambda)-(0.5*qchisq(1-alpha, 1)+len*log(mean(x))+len)/len
+CI_LRT <- c(uniroot(f, c(0,0.05), tol=0.0001)$root, uniroot(f, c(0.05,0.15), tol=0.0001)$root)
 # CI from Wald Tests
 CI_Wald <- c((1 - qnorm(1 - alpha/2)/sqrt(len)) / mean(x), (1 + qnorm(1 - alpha/2)/sqrt(len)) / mean(x))
 # CI from Score Tests
 CI_Score <- c((1 - qnorm(1 - alpha/2)/sqrt(len)) / mean(x), (1 + qnorm(1 - alpha/2)/sqrt(len)) / mean(x))
+
+
+## Draw line to specify the interval of root
+dd <- seq(0, 0.25, by=0.000001)
+tt <- f(dd)
+df <- data.frame(dd,tt)
+g <- ggplot(df,aes(dd,tt))
+g <- g+geom_line(col='red')   #red line
+g
--- assignment4/code/Q4.R
+++ assignment4/code/Q4.R
@@ -14,3 +14,21 @@ b_jack <- (theta_hat/m * sum(y-mean(y)) - (1/n) * sum(x-mean(x))) / mean(y)
 v_jack <- (1/(n*(n-1)) * sum((x-mean(x))^2) + theta_hat^2/(m*(m-1)) * sum((y-mean(y))^2)) / mean(y)^2
 
 # bootstrap
+r <- 1000
+f <- function(x, y, r){
+  mean_x <- mean(sample(x,length(x),replace = TRUE))
+  mean_y <- mean(sample(y, length(y), replace = TRUE))
+  theta <- mean_x / mean_y
+  for(i in 2:r) {
+    a <- mean(sample(x,length(x),replace = TRUE)) / mean(sample(y, length(y), replace = TRUE))
+    theta <- c(theta, a)
+  }
+  theta_star <- theta
+}
+
+theta_star <- f(x, y, r)
+  
+b_boot <- (1/r) * sum(theta_star - theta_hat)
+v_boot <- 1 / (r-1) * sum(theta_star - mean(theta_star))
+
+  
